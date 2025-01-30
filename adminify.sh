a418edbc697eebee48a2afed09502afc92c33cfd
Phil Wilson <pgw@acm.org>
2025-01-30 12:06:31 +0000

    Update adminify.sh

--- adminify.sh
+++ adminify.sh
@@ -1,3 +1,21 @@
-echo $1
-dseditgroup -o edit -a $1 -t user admin
-dseditgroup -o edit -a $1 -t user wheel
+USERNAME=$1
+
+if id "$USERNAME" > /dev/null 2>&1; then
+    echo "User $USERNAME exists."
+else
+    echo "User $USERNAME does not exist."
+    exit 0
+fi
+
+GROUPNAME=admin
+
+if groups "$USERNAME" | grep -qw "$GROUPNAME"; then
+    echo "User $USERNAME is in group $GROUPNAME."
+
+else
+    echo "User $USERNAME is NOT in group $GROUPNAME."
+    echo "Elevating..."
+    dseditgroup -o edit -a $USERNAME -t user admin
+    dseditgroup -o edit -a $USERNAME -t user wheel
+fi
+
